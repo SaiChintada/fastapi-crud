ed51c9dbbd331eb458fa6d66931f9e3b58c0940a
SaiChintada <chintadasai51758@gmail.com>
2026-04-07 15:37:46 +0530

    Added update and delete APIs

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 def get_db():
     db = SessionLocal()
     try:
@@ -35,3 +45,31 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
 @app.get("/items/", response_model=list[schemas.ItemResponse])
 def read_items(db: Session = Depends(get_db)):
     return db.query(models.Item).all()
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if not db_item:
+        return {"error": "Item not found"}
+
+    db_item.name = item.name
+    db_item.description = item.description
+
+    db.commit()
+    db.refresh(db_item)
+
+    return db_item
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if not db_item:
+        return {"error": "Item not found"}
+
+    db.delete(db_item)
+    db.commit()
+
+    return {"message": "Item deleted successfully"}
